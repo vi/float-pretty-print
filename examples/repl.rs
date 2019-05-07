f2b78e20b72a481a538db0327bb90028f4f84677
Vitaly _Vi Shukela <vi0oss@gmail.com>
2019-05-07 05:59:19 +0300

    Fill in details, 1.23 compat

--- examples/repl.rs
+++ examples/repl.rs
@@ -2,7 +2,7 @@ extern crate float_pretty_print;
 
 use std::io::BufRead;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     eprintln!("Type number to be formatted using float_pretty_print");
     eprintln!("Also type `width=<number>` or `prec=<number>` to set width or precision.");
 
@@ -14,16 +14,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut prec : Option<usize> = None;
 
     for l in si.lines() {
-        let l = l?;
+        let l = l.unwrap();
         if l.contains('=') {
             let before_and_after_eq_sign = l.split('=').collect::<Vec<_>>();
             assert_eq!(before_and_after_eq_sign.len(), 2);
             match before_and_after_eq_sign[0] {
                 "width" => {
-                    width = Some(before_and_after_eq_sign[1].parse()?);
+                    width = Some(before_and_after_eq_sign[1].parse().unwrap());
                 },
                 "prec" => {
-                    prec = Some(before_and_after_eq_sign[1].parse()?);
+                    prec = Some(before_and_after_eq_sign[1].parse().unwrap());
                 },
                 _ => {
                     eprintln!("Unknown property");
@@ -31,7 +31,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
             continue;
         }
-        let f : f64 = l.parse()?;
+        let f : f64 = l.parse().unwrap();
         let f = float_pretty_print::PrettyPrintFloat(f);
 
         match (width, prec) {
@@ -41,5 +41,4 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             (Some(w), Some(p)) => println!("{:w$.p$}", f, w=w, p=p),
         }
     }
-    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,30 @@
 //! Only two formatting parameters are supported:
 //!
 //! * width is minimum width
-//! * precision is maximum width. It is recommended to set them to the same value
+//! * precision is maximum width
 //!
 //! `###` is printed if it can't output the number with given constraint.
+//!
+//! Example:
+//!
+//! ```
+//!   use float_pretty_print::PrettyPrintFloat;
+//!   assert_eq!(format!("{}", PrettyPrintFloat(3.45)), "3.45");
+//!   assert_eq!(format!("{}", PrettyPrintFloat(12.0)), "12.0");
+//!   assert_eq!(format!("{}", PrettyPrintFloat(120000000.0)), "1.2e8");
+//!   assert_eq!(format!("{:5.5}", PrettyPrintFloat(12345.0)), "12345");
+//!   assert_eq!(format!("{:5.5}", PrettyPrintFloat(12.345)), "12.35");
+//!   assert_eq!(format!("{:5.5}", PrettyPrintFloat(0.12345)), "0.123");
+//!   assert_eq!(format!("{:5.5}", PrettyPrintFloat(1234500000.0)), "1.2e9");
+//!   assert_eq!(format!("{:5.5}", PrettyPrintFloat(12345.0e-19)), "1e-15");
+//!   assert_eq!(format!("{:5.5}", PrettyPrintFloat(12345.0e-100)), "1e-96");
+//!   assert_eq!(format!("{:5.5}", PrettyPrintFloat(12345.0e-130)), "    0");
+//!   assert_eq!(format!("{:5.5}", PrettyPrintFloat(12345.0e+130)), "1e134");
+//!   assert_eq!(format!("{:4.4}", PrettyPrintFloat(12345.0e+130)), "####");
+//!   assert_eq!(format!("{:6.6}", PrettyPrintFloat(12345.0e-130)), "1e-126");
+//! ```
+//!
+//! Supports even Rust 1.23
 
 use std::fmt::{Display, Formatter, Result};
 
@@ -22,19 +43,7 @@ const DEBUG : bool = false;
 ///
 /// ```
 ///   use float_pretty_print::PrettyPrintFloat;
-///   assert_eq!(format!("{}", PrettyPrintFloat(3.45)), "3.45");
-///   assert_eq!(format!("{}", PrettyPrintFloat(12.0)), "12.0");
-///   assert_eq!(format!("{}", PrettyPrintFloat(120000000.0)), "1.2e8");
-///   assert_eq!(format!("{:5.5}", PrettyPrintFloat(12345.0)), "12345");
-///   assert_eq!(format!("{:5.5}", PrettyPrintFloat(12.345)), "12.35");
-///   assert_eq!(format!("{:5.5}", PrettyPrintFloat(0.12345)), "0.123");
-///   assert_eq!(format!("{:5.5}", PrettyPrintFloat(1234500000.0)), "1.2e9");
-///   assert_eq!(format!("{:5.5}", PrettyPrintFloat(12345.0e-19)), "1e-15");
-///   assert_eq!(format!("{:5.5}", PrettyPrintFloat(12345.0e-100)), "1e-96");
-///   assert_eq!(format!("{:5.5}", PrettyPrintFloat(12345.0e-130)), "    0");
-///   assert_eq!(format!("{:5.5}", PrettyPrintFloat(12345.0e+130)), "1e134");
-///   assert_eq!(format!("{:4.4}", PrettyPrintFloat(12345.0e+130)), "####");
-///   assert_eq!(format!("{:6.6}", PrettyPrintFloat(12345.0e-130)), "1e-126");
+///   assert_eq!(format!("{:4.4}", PrettyPrintFloat(0.00005)), "5e-5");
 /// ```
 pub struct PrettyPrintFloat(pub f64);
 
