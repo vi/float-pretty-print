e47ad3220587f25a1d6aa83c3dd6f069b7c67d28
Vitaly _Vi Shukela <vi0oss@gmail.com>
2019-05-07 05:46:17 +0300

    Doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,41 @@
+#![deny(missing_docs)]
+#![forbid(unsafe_code)]
+
+//! Round and format f64 number for showing it to humans, with configurable minimum and maximum width.
+//! It automatically switches between exponential and usual forms as it sees fit.
+//!
+//! It works by trying usual `format!`, possibly multiple times and inspecting the resulting string.
+//!
+//! Only two formatting parameters are supported:
+//!
+//! * width is minimum width
+//! * precision is maximum width. It is recommended to set them to the same value
+//!
+//! `###` is printed if it can't output the number with given constraint.
+
 use std::fmt::{Display, Formatter, Result};
 
 const DEBUG : bool = false;
 
+/// `f64` wrapper to use with formatting code.
+/// See the crate-level doc for details.
+///
+/// ```
+///   use float_pretty_print::PrettyPrintFloat;
+///   assert_eq!(format!("{}", PrettyPrintFloat(3.45)), "3.45");
+///   assert_eq!(format!("{}", PrettyPrintFloat(12.0)), "12.0");
+///   assert_eq!(format!("{}", PrettyPrintFloat(120000000.0)), "1.2e8");
+///   assert_eq!(format!("{:5.5}", PrettyPrintFloat(12345.0)), "12345");
+///   assert_eq!(format!("{:5.5}", PrettyPrintFloat(12.345)), "12.35");
+///   assert_eq!(format!("{:5.5}", PrettyPrintFloat(0.12345)), "0.123");
+///   assert_eq!(format!("{:5.5}", PrettyPrintFloat(1234500000.0)), "1.2e9");
+///   assert_eq!(format!("{:5.5}", PrettyPrintFloat(12345.0e-19)), "1e-15");
+///   assert_eq!(format!("{:5.5}", PrettyPrintFloat(12345.0e-100)), "1e-96");
+///   assert_eq!(format!("{:5.5}", PrettyPrintFloat(12345.0e-130)), "    0");
+///   assert_eq!(format!("{:5.5}", PrettyPrintFloat(12345.0e+130)), "1e134");
+///   assert_eq!(format!("{:4.4}", PrettyPrintFloat(12345.0e+130)), "####");
+///   assert_eq!(format!("{:6.6}", PrettyPrintFloat(12345.0e-130)), "1e-126");
+/// ```
 pub struct PrettyPrintFloat(pub f64);
 
 #[derive(PartialEq, Eq, Debug)]
